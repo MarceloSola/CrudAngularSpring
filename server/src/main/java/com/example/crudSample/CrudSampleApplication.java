5123496bcf5f4fa1a75bf4115ea632cee2f96a6e
MarceloSola <38148447+MarceloSola@users.noreply.github.com>
2018-05-24 01:54:44 -0300

    Server

---
+++ server/src/main/java/com/example/crudSample/CrudSampleApplication.java
@@ -0,0 +1,29 @@
+package com.example.crudSample;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class CrudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSampleApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository repository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+				Car car = new Car();
+				car.setName(name);
+				repository.save(car);
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}
+}
